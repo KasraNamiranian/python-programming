70a58efb4f80e8c02bcd1ae9cc0c5d6f44518824
KasraNamiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-06-06 00:25:03 +0330

    Add files via upload

---
+++ 3-3.py
@@ -0,0 +1,40 @@
+def drawingshapes():
+    print("+" ,"- "*4 , end="")
+    print("+" ,"- "*4 ,end="")
+    print("+")
+    print("|", " "*7 , "|"," "*7 , "|")
+    print("|", " "*7 , "|"," "*7 , "|")
+    print("|", " "*7 , "|"," "*7 , "|")
+    print("|", " "*7 , "|"," "*7 , "|")
+
+drawingshapes()
+drawingshapes()
+print("+" ,"- "*4 , end="")
+print("+" ,"- "*4 ,end="")
+print("+")
+
+
+
+print("-----------------------------------")
+
+def drawingshapes4():
+    print("+" ,"- "*4 , end="")
+    print("+" ,"- "*4 ,end="")
+    print("+" ,"- "*4 , end="")
+    print("+" ,"- "*4 ,end="")
+    print("+")
+    print("|", " "*7 , "|"," "*7 , "|", " "*7, "|", " "*7,"|", " "*7)
+    print("|", " "*7 , "|"," "*7 , "|", " "*7, "|", " "*7,"|", " "*7)
+    print("|", " "*7 , "|"," "*7 , "|", " "*7, "|", " "*7,"|", " "*7)
+    print("|", " "*7 , "|"," "*7 , "|", " "*7, "|", " "*7,"|", " "*7)
+drawingshapes4()
+drawingshapes4()
+drawingshapes4()
+drawingshapes4()
+print("+" ,"- "*4 , end="")
+print("+" ,"- "*4 ,end="")
+print("+" ,"- "*4 , end="")
+print("+" ,"- "*4 ,end="")
+print("+")
+
+
