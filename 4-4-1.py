6bdba6e2866869d13ca7bf69dab54d59805d32e1
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-08-13 00:12:14 +0330

    Add files via upload

---
+++ 4-4-1.py
@@ -0,0 +1,20 @@
+import turtle
+import math
+
+
+def polygon(t,n,length):
+    deg=360/n #andaze har zavie kharejie yek chand zeli montazam
+    for i in range(n):
+        t.fd(length)
+        t.lt(deg)
+
+
+def circle(t , r):
+    circumference = 2 * math.pi * r
+    n = int(circumference / 3) + 1
+    length = circumference / n
+    polygon(t , n , length)
+
+bob=turtle.Turtle()
+circle(bob,50)
+turtle.mainloop()
